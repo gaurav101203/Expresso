a7fd2aeff79b5b05cb7a4c72959c4c79d5e9c4f8
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2024-12-22 14:09:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -107,6 +107,6 @@ io.on('connection', (socket) => {
 });
 
 // Start server and WebSocket
-server.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
